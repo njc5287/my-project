8b24083a5069161d435eda852448672f1d1abd3b
njc5287 <125466522+njc5287@users.noreply.github.com>
2024-04-25 16:28:29 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Movie from './Movie';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Movie.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-lg">
+      <h3>List of Movies</h3>
+      <div className="row">
+      <div className="col-lg-4 col-md-7 col-sm-3">
+        <Movie />
+        </div>
+      <div className="col-lg-4 col-md-7 col-sm-3">
+        <Movie />
+        </div>
+      <div className="col-lg-4 col-md-7 col-sm-3">
+      <Movie />
     </div>
+    </div>
+    </div>
+  
   );
-}
+};
 
 export default App;
---
+++ src/Movie.js
@@ -0,0 +1,24 @@
+
+ 
+    import React from 'react';
+
+    const Movie = () => {
+      return (
+        <div className='greybox'>
+        <div className='movie'>
+          <div className="movie-title">
+            The Iron Giant          
+        </div>
+        <div className="movie-director">
+            Director: Brad Bird
+        </div>
+
+        <div className="movie-actor">
+            Actors: Eli Marienthal, Harry Connick Junior, Jennifer Aniston
+        </div>
+        </div>
+        </div>
+      );
+    };
+    
+    export default Movie;
